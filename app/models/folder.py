2b8fa8890d2191399bdf958e3977f0913927c0fb
Christopher Simpson <simpsonc6@winthropalumni.com>
2023-07-12 12:06:14 -0400

    Folder model initalized

---
+++ app/models/folder.py
@@ -0,0 +1,27 @@
+from .db import db, environment, SCHEMA
+
+
+class Folder(db.Model):
+    __tablename__ = 'Folders'
+
+    if environment == "production":
+        __table_args__ = {'schema':SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable = False)
+    title = db.Column(db.String(50), nullable = False)
+    description = db.Column(db.String(2000), nullable = False)
+    is_public = db.Column(db.Boolean, nullable = False, default=False)
+
+    user = db.relationship("User", back_populates="Folders")
+
+    def to_dict(self):
+        return {
+            "id":self.id,
+            "user_id":self.user_id,
+            "title":self.title,
+            "description":self.description,
+            "is_public":self.is_public,
+        }
+
+
