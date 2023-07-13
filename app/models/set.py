e68b1a4e164c96f693c58c3102cac8c36bbdc11b
Christopher Simpson <simpsonc6@winthropalumni.com>
2023-07-13 00:04:13 -0400

    fixes for postgres

--- app/models/set.py
+++ app/models/set.py
@@ -7,7 +7,7 @@ class Set(db.Model):
         __table_args__ = {'schema':SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("Users.id")))
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
     folder_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("Folders.id")))
     title = db.Column(db.String(50), nullable = False)
     description = db.Column(db.String(2000), nullable = False)
