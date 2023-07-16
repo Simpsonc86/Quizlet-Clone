ed41aa0ef894221ac3b2084be7b3727933239b38
Christopher Simpson <simpsonc6@winthropalumni.com>
2023-07-15 23:18:16 -0400

    Added dashboard and get one folder to store

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,6 +6,7 @@ import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 import CreateFolder from "./components/CreateFolder";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   const dispatch = useDispatch();
@@ -24,6 +25,9 @@ function App() {
           </Route>
           <Route path="/signup">
             <SignupFormPage />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
           </Route>
           <Route path="/new-folder">
             <CreateFolder />
