fd0f100e804236c0bf410cdfd8cf4779184135a5
Christopher Simpson <simpsonc6@winthropalumni.com>
2023-08-09 20:48:57 -0400

    fixed bug on delete set from set page

--- react-app/src/components/DeleteSetModal/index.js
+++ react-app/src/components/DeleteSetModal/index.js
@@ -2,21 +2,24 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import{useModal} from "../../context/Modal"
 // import { deleteFolderThunk } from "../../store/folders";
-// import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 // import { getAllFoldersThunk } from "../../store/folders";
 import { deleteSetThunk, getAllSetsThunk } from "../../store/sets";
 
 export default function DeleteSetModal({setId,folderId}){
     const dispatch = useDispatch();
-    // const history = useHistory();
+    const history = useHistory();
+    // const {folder_id, set_id} = useParams();
     const {closeModal}= useModal();
+
+    console.log('objects from props and params SET:', setId, " FOLDER: ", folderId);
 
     const handleSubmit = async (e) =>{
         e.preventDefault();
-        dispatch(deleteSetThunk(setId))
+        await dispatch(deleteSetThunk(setId))
         .then(dispatch(getAllSetsThunk()))
         .then(closeModal())
-        // history.push(`/folders/${folderId}`)
+        history.push(`/folders/${folderId}`)
     };
 
     return(
